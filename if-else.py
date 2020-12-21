217093529313cb35ce96c436d60e5646298570a1
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-21 15:10:19 +0100

    if-else challenge

---
+++ if-else.py
@@ -0,0 +1,8 @@
+# HackerRank https://www.hackerrank.com/challenges/py-if-else/problem
+
+n = 5
+
+if n % 2 != 0 or ((n % 2 == 0) and (6 <= n <= 20)):
+    print("Weird")
+elif n % 2 == 0 and ((2 <= n <= 5) or (n > 20)):
+    print("Not Weird")
