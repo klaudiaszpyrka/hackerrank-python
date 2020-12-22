db6f5b334c6edc69a71ed6b17ba01aaee3be8886
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-22 18:50:07 +0100

    coordinates challenge

---
+++ scripts/coordinates-permutations.py
@@ -0,0 +1,18 @@
+# HackerRank https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+x = 1
+y = 1
+z = 1
+n = 2
+
+# coordinates = []
+
+# for i in range(x+1):
+#    for j in range(y+1):
+#        for k in range(z+1):
+#            coordinates.append([i, j, k])
+
+coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)]
+valid_permutations = [c for c in coordinates if sum(c) != n]
+
+print(valid_permutations)
