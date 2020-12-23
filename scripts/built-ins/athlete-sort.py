33f8fe58dcb99c7e9b0036dcd3b9f834c49e64b8
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-23 14:44:15 +0100

    add new challenges and reorganize

---
+++ scripts/built-ins/athlete-sort.py
@@ -0,0 +1,27 @@
+# HackerRank https://www.hackerrank.com/challenges/python-sort-sort/problem
+
+# Sample input:
+# 5 3
+# 10 2 5
+# 7 1 0
+# 9 9 9
+# 1 23 12
+# 6 5 9
+# 1
+
+nm = input().split()
+n = int(nm[0])
+m = int(nm[1])
+
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+
+k = int(input())
+
+# sort an object using some of object's indices as keys
+arr_sorted = sorted(arr, key=lambda arr_unsorted: arr_unsorted[k])
+
+for a in arr_sorted:
+    print(*a)  # print the elements of a list separated by space on one line
---
+++ scripts/built-ins/zipped.py
@@ -0,0 +1,21 @@
+# HackerRank https://www.hackerrank.com/challenges/zipped/problem
+
+# Sample input:
+# 5 3
+# 89 90 78 93 80
+# 90 91 85 88 86
+# 91 92 83 89 90.5
+
+x = int(input().split()[1])
+
+student_marks = []
+
+for _ in range(x):
+    line = list(map(float, input().split()))
+    student_marks.append(line)
+
+# print(student_marks)
+# print(list(zip(*student_marks)))
+
+for element in zip(*student_marks):
+    print("{:.1f}".format(sum(element) / len(element)))
